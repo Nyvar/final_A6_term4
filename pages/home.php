5b2b65d1c5af67abb6aa3311ae435291dd3b34ca
Nyvar <nyvarchan78@gmail.com>
2026-05-02 23:39:07 +0700

    include leftnav

--- pages/home.php
+++ pages/home.php
@@ -1,2 +1,27 @@
-<h1 class="text-center mt-5">Welcome to the Expense Tracker</h1>
-<p>I am nyvar</p>
+<div class="container">
+  <!-- Left navigation -->
+  <div id="navPanel" class="nav-panel">
+    <?php include 'components/leftnavigation.php'; ?>
+  </div>
+
+  <!-- Main content -->
+  <div class="main">
+    <h1 class="text-center mt-5">Welcome to the Expense Tracker</h1>
+    
+  </div>
+</div>
+
+<style>
+    .container{
+        display:flex;
+        height:100vh;
+    }
+    .main{
+        flex:1;
+       
+    }
+    .nav-panel{
+        display:none;
+    }
+    
+</style>
