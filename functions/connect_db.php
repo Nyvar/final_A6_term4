0b794e6524aa2e77c6ab80307014508c57359c41
IntroSeth2299 <sounpiseth2299@gmail.com>
2026-05-11 01:45:51 -0700

    changes only style.css

--- functions/connect_db.php
+++ functions/connect_db.php
@@ -3,9 +3,9 @@ function connect_db(): PDO {
     $host = 'localhost';
     $db = 'expense_tracker_2026';
     $user = 'root';
-    $pass = 'Step@2025';
+    $pass = '';
     $charset = 'utf8mb4';
-    $port = 3388;
+    $port = 3306;
     $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
     try {
         $pdo = new PDO($dsn, $user, $pass);
